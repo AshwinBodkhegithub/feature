8a1d87ea341d86aa3ab15e5936cf217198062a73
Ashwin <ashwinvedika1918@gmail.com>
2021-08-08 00:04:00 +0530

    please commit this

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -2,13 +2,14 @@ package com.in28minutes.maven;
 
 /**
  * Hello World test!
- *
+ * This is change by Ashwin
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  Test Test Test" );
+
+        System.out.println( "Ashwin is testing this please do the changes accordingly" );
     }
 
 	public int calculateSomething() {
